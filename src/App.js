79a5a98b485842f6f50186c767b1ed1eb909f97b
Aditya Solanki <aditya2ss283@gmail.com>
2021-10-29 14:34:24 +0530

    wip: update App

--- src/App.js
+++ src/App.js
@@ -2,21 +2,46 @@ import "./App.css";
 import SignUp from "./pages/Signup";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
+import Create from "./pages/CreateGroup";
+import EditProfile from "./pages/EditProfile";
 import { Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import PrivateRoute from "./routes/PrivateRoute";
+import UserGroups from "./pages/UserGroups";
 
 function App() {
-  return (
-    <div className="App">
-      {
-        <Routes>
-          <Route path="/" element={<LandingPage></LandingPage>}></Route>
-          <Route path="/signup" element={<SignUp></SignUp>}></Route>
-          <Route path="/login" element={<Login></Login>}></Route>
-          <PrivateRoute path="/home" element={<Home></Home>}></PrivateRoute>
-        </Routes>
-      }
+    return (
+        <div className="App">
+        {<Routes>
+            <Route 
+                exact path="/" 
+                element={<LandingPage/>}>
+            </Route>
+            <Route 
+                path="/signup" 
+                element={<SignUp/>}>    
+            </Route>
+            <Route 
+                path="/login" 
+                element={<Login/>}>
+            </Route>
+            <PrivateRoute 
+                path="/home" 
+                element={<Home/>}>
+            </PrivateRoute>
+            <PrivateRoute 
+                path="/create" 
+                element={<Create/>}>
+            </PrivateRoute>
+            <PrivateRoute 
+                path="/user-groups" 
+                element={<UserGroups/>}>
+            </PrivateRoute>
+            <PrivateRoute 
+                path="/edit-profile" 
+                element={<EditProfile/>}>
+            </PrivateRoute>
+        </Routes>}
     </div>
   );
 }
