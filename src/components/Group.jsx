0a7c0ff5a2ab8635044e68f0c230d59892ba8281
Aditya Solanki <aditya2ss283@gmail.com>
2021-10-29 21:45:51 +0530

    wip: added dark mode toggle on the main page

---
+++ src/components/Group.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import SideMenu from './SideMenu';
+
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../contexts/AuthProvider";
+import { Checkbox } from "@mui/material";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+
+export default function Group() {
+
+    const { currentUser } = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    const { modeStyle, theme, setTheme } = useContext(AuthContext);
+
+    return (
+        <div className="Page-wrapper">
+            <div className="Page-nav">
+                <SideMenu/>
+            </div>
+            <div className="Page-main">
+                {" "}
+                <div style={{ textAlign: "right" }}>
+                    {" "}
+                    <Checkbox
+                        icon={<DarkModeOutlinedIcon />}
+                        checkedIcon={<DarkModeIcon />}
+                        onChange={() => {
+                            theme === "LIGHT" ? setTheme("DARK") : setTheme("LIGHT");
+                        }}
+                        sx={{ margin: "1rem" }}
+                    />
+                </div>
+                <h1>Ayyo my G</h1>
+            </div>
+        </div>
+    )
+}
