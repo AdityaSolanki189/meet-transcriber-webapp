641cd8dfac9c251414e2f3103c5dfcc0490d3759
AdityaNair102001 <adityanair102001@gmail.com>
2021-10-30 18:17:46 +0530

    getUserFromDb added

--- src/config/Firebase.js
+++ src/config/Firebase.js
@@ -2,18 +2,18 @@ import firebase from "firebase/compat/app"; //using compat in the new versions i
 import "firebase/compat/firestore";
 import "firebase/compat/auth";
 
-import { initializeApp } from "firebase/app";
-import { getAuth, onAuthStateChanged } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import {initializeApp} from "firebase/app";
+import {getAuth, onAuthStateChanged} from "firebase/auth";
+import {getFirestore} from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDuOd8iJvNJi1LYGeeVjZXrqFrhDUV-RWU",
-  authDomain: "meet-transcriber.firebaseapp.com",
-  projectId: "meet-transcriber",
-  storageBucket: "meet-transcriber.appspot.com",
-  messagingSenderId: "954018788223",
-  appId: "1:954018788223:web:647f6d0ba2636b7dcc604a",
-  measurementId: "G-5S28K0FPS3",
+    apiKey: "AIzaSyDuOd8iJvNJi1LYGeeVjZXrqFrhDUV-RWU",
+    authDomain: "meet-transcriber.firebaseapp.com",
+    projectId: "meet-transcriber",
+    storageBucket: "meet-transcriber.appspot.com",
+    messagingSenderId: "954018788223",
+    appId: "1:954018788223:web:647f6d0ba2636b7dcc604a",
+    measurementId: "G-5S28K0FPS3"
 };
 
 // firebase.initializeApp(firebaseConfig);
@@ -23,4 +23,4 @@ const firebaseApp = initializeApp(firebaseConfig);
 const auth = getAuth(firebaseApp);
 const db = getFirestore();
 
-export { auth, db, onAuthStateChanged };
+export {auth, db, onAuthStateChanged};
--- src/contexts/AuthProvider.js
+++ src/contexts/AuthProvider.js
@@ -1,129 +1,139 @@
-import { createContext, useEffect, useState } from "react";
+import {createContext, useEffect, useState} from "react";
 
-import { auth, onAuthStateChanged, db } from "../config/Firebase";
-import { collection, addDoc } from "@firebase/firestore";
+import {auth, onAuthStateChanged, db} from "../config/Firebase";
+import {collection, addDoc} from "@firebase/firestore";
+import {doc, getDoc} from "firebase/firestore";
 
-import {
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  signOut,
-} from "firebase/auth";
-import { useNavigate } from "react-router-dom";
-import { colours } from "../theme/colors";
+import {createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut} from "firebase/auth";
+import {useNavigate} from "react-router-dom";
+import {colours} from "../theme/colors";
 
 export const AuthContext = createContext();
 
-export default function AuthContextProvider({ children }) {
-  const [currentUser, setCurrentUser] = useState(
-    localStorage.getItem("userEmail")
-  );
-  const [error, setError] = useState("");
-  const [loading, setLoading] = useState(false);
+export default function AuthContextProvider({children}) {
+    const [currentUser,
+        setCurrentUser] = useState(localStorage.getItem("userEmail"));
+    const [error,
+        setError] = useState("");
+    const [loading,
+        setLoading] = useState(false);
 
-  const [theme, setTheme] = useState("LIGHT");
+    const [theme,
+        setTheme] = useState("LIGHT");
 
-  const modeStyle =
-    theme === "LIGHT"
-      ? {
-          backgroundColor: "white",
-          elementBackgroundColor: "white",
-          placeholderColor: colours.grey,
-          textColor: "black",
+    const modeStyle = theme === "LIGHT"
+        ? {
+            backgroundColor: "white",
+            elementBackgroundColor: "white",
+            placeholderColor: colours.grey,
+            textColor: "black"
         }
-      : {
-          backgroundColor: "black",
-          elementBackgroundColor: "black",
-          placeholderColor: colours.blue,
-          textColor: "white",
-          subTextColor: colours.lightGray,
+        : {
+            backgroundColor: "black",
+            elementBackgroundColor: "black",
+            placeholderColor: colours.blue,
+            textColor: "white",
+            subTextColor: colours.lightGray
         };
 
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  async function postUserToDb(email, UID) {
-    try {
-      const docRef = await addDoc(collection(db, "users"), {
-        email: email,
-        UID: UID,
-      });
-      console.log("user added with docID", docRef.id);
-    } catch (err) {
-      console.error("Error adding document: ", err);
+    async function postUserToDb(email, UID) {
+        try {
+            const docRef = await addDoc(collection(db, "users"), {
+                email: email,
+                UID: UID
+            });
+            console.log("user added with docID", docRef.id);
+        } catch (err) {
+            console.error("Error adding document: ", err);
+        }
     }
-  }
+    async function getUserFromDb(UID) {
+        try {
+            const docRef = doc(db, "cities", "SF");
+            const docSnap = await getDoc(docRef);
 
-  async function signUp(email, password) {
-    try {
-      setError("");
-      setLoading(true);
-      const response = await createUserWithEmailAndPassword(
-        auth,
-        email,
-        password
-      );
-      postUserToDb(email, response.user.uid);
-      navigate("/login");
-    } catch (err) {
-      console.log(err);
-      setError(err.message);
+            if (docSnap.exists()) {
+                console.log("Document data:", docSnap.data());
+                return docSnap.data();
+            } else {
+                // doc.data() will be undefined in this case
+                console.log("No such document!");
+            }
+        } catch (err) {
+            console.log(err)
+        }
     }
-    setLoading(false);
-  }
 
-  async function logIn(email, password) {
-    try {
-      setLoading(true);
-      setError("");
-      const response = await signInWithEmailAndPassword(auth, email, password);
-      localStorage.setItem("userEmail", response.user.email);
-      navigate("/home");
-      console.log(response);
-    } catch (err) {
-      console.log(err);
-      setError(err.message);
+    async function signUp(email, password) {
+        try {
+            setError("");
+            setLoading(true);
+            const response = await createUserWithEmailAndPassword(auth, email, password);
+            postUserToDb(email, response.user.uid);
+            navigate("/login");
+        } catch (err) {
+            console.log(err);
+            setError(err.message);
+        }
+        setLoading(false);
     }
-    setLoading(false);
-  }
 
-  async function logout() {
-    try {
-      setLoading(true);
-      setError("");
-      const response = signOut(auth);
-      console.log(response);
-    } catch (err) {
-      console.log(err);
-      setError(err.message);
+    async function logIn(email, password) {
+        try {
+            setLoading(true);
+            setError("");
+            const response = await signInWithEmailAndPassword(auth, email, password);
+            localStorage.setItem("userEmail", response.user.email);
+            navigate("/home");
+            console.log(response);
+        } catch (err) {
+            console.log(err);
+            setError(err.message);
+        }
+        setLoading(false);
     }
-    setLoading(false);
-  }
 
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      setCurrentUser(user);
-      console.log(" at authProvider use Effect", user);
-    });
-    return unsubscribe;
-  }, []);
+    async function logout() {
+        try {
+            setLoading(true);
+            setError("");
+            const response = signOut(auth);
+            console.log(response);
+        } catch (err) {
+            console.log(err);
+            setError(err.message);
+        }
+        setLoading(false);
+    }
 
-  return (
-    <AuthContext.Provider
-      value={{
-        currentUser,
-        setCurrentUser,
-        signUp,
-        error,
-        loading,
-        setError,
-        setLoading,
-        logIn,
-        logout,
-        modeStyle,
-        theme,
-        setTheme,
-      }}
-    >
-      {!loading && children}
-    </AuthContext.Provider>
-  );
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            setCurrentUser(user);
+            console.log(" at authProvider use Effect", user);
+        });
+        return unsubscribe;
+    }, []);
+
+    return (
+        <AuthContext.Provider
+            value={{
+            currentUser,
+            setCurrentUser,
+            signUp,
+            error,
+            loading,
+            setError,
+            setLoading,
+            logIn,
+            logout,
+            modeStyle,
+            theme,
+            setTheme,
+			getUserFromDb
+        }}>
+            {!loading && children}
+        </AuthContext.Provider>
+    );
 }
