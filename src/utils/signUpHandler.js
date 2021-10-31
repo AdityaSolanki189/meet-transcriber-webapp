d4cc5047ae2a62e5ebd805b09408e3ce1d62ede6
AdityaNair102001 <adityanair102001@gmail.com>
2021-10-31 12:06:20 +0530

    set UID to doc ID

--- src/utils/signUpHandler.js
+++ src/utils/signUpHandler.js
@@ -1,17 +1,9 @@
-export default function signUpHandler(
-  signUp,
-  password,
-  confirmPassword,
-  email,
-  username,
-  setError,
-  setLoading
-) {
-  if (password === confirmPassword) {
-    signUp(username, email, password);
-  } else {
-    setError("Passwords do not match");
-    setLoading(false);
-    return;
-  }
+export default function signUpHandler(signUp, password, confirmPassword, email, username, setError, setLoading,) {
+    if (password === confirmPassword) {
+        signUp(email, password, username);
+    } else {
+        setError("Passwords do not match");
+        setLoading(false);
+        return;
+    }
 }
