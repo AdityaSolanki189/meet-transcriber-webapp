db3e1d4a43831507f90f983c46216252a72344a9
anuraggpatil <anuraggp2001@gmail.com>
2021-10-31 12:06:35 +0530

    changes to group

--- src/components/Group.jsx
+++ src/components/Group.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import SideMenu from './SideMenu';
+import './Group.css';
 
 import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
@@ -7,18 +8,32 @@ import { AuthContext } from "../contexts/AuthProvider";
 import { Checkbox } from "@mui/material";
 import DarkModeIcon from "@mui/icons-material/DarkMode";
 import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import editLogo from '../resources/edit.png';
+import calendar from '../resources/calendar-logo.png';
+import clock from '../resources/clock-logo.png';
 
 export default function Group() {
 
     const { currentUser } = useContext(AuthContext);
     const navigate = useNavigate();
-
     const { modeStyle, theme, setTheme } = useContext(AuthContext);
+
+
+    const timestamp = 'Mon, 10/25 - 11:08 AM'
+    const meetLength = '2.01';
+    const speakersList = ['Anurag Patil', 'Aditya Solanki', 'Aditya Nair'];
+    var html = '';
+    for (var i = 0; i < speakersList.length; i++) {
+        if(i !== speakersList.length - 1)
+            html += speakersList[i] + ', ';
+        else
+            html += speakersList[i];
+    }
 
     return (
         <div className="Page-wrapper">
             <div className="Page-nav">
-                <SideMenu/>
+                <SideMenu />
             </div>
             <div className="Page-main">
                 {" "}
@@ -33,7 +48,30 @@ export default function Group() {
                         sx={{ margin: "1rem" }}
                     />
                 </div>
-                <h1>Ayyo my G</h1>
+                <div className="header">
+                    <div className="heading">
+                        <div id="meet-name">
+                            <p>Google Meet - Mon,  Oct 25,  2021 at 11:08 am</p>
+                        </div>
+                        <div id="edit-image">
+                            <img src={editLogo} />
+                        </div>
+                    </div>
+                    <div className="date-n-time">
+                        <img src={calendar} />
+                        <p>{timestamp}</p>
+                        <img src={clock} />
+                        <p>{meetLength}</p>
+                    </div>
+                    <p id="speaker">SPEAKERS</p>
+                    <div className="speakers-list">
+                        <p>{html}</p>
+                    </div>
+                </div>
+                <div className="recycler-view">
+                    
+                </div>
+
             </div>
         </div>
     )
---
+++ src/components/Transcript.js
@@ -0,0 +1,39 @@
+const Transcript = () => {
+
+    const data = [
+        {
+            name: "Anurag Patil",
+            shitHeSaid: "Ricardo rheticus. As Meru gira recorded earlier. They are Jesse Connor, can you do three second Roger decry 00072 They gotta get their current job maybe Ruka are a bit more fixable, and again, They are maybe a bit go away. Avi Taha Tommaso terasaki fnsa Whatever dismembered petani of Nagasaki was the border so inconsistent to get in cases the problem",
+            timeStamp: 0
+        },
+        {
+            name: "Aditya Solanki",
+            shitHeSaid: "might have to get old Ricky, aka Casentino analysis during this video,",
+            timeStamp: 1
+        },
+        {
+            name: "Aditya Nair",
+            shitHeSaid: "push me to SEMA. Soto lucky, Jessica over in Korea Jody Barlow is dibber footstone as a highlight specificall thank you so much RBA carbon capture option open directly open in approximately one wood area to a niche agency the key highlight your tag which is a sample highlight the sector.",
+            timeStamp: 2
+        },
+        {
+            name: "Anurag Patil",
+            shitHeSaid: "Gay important to highlight what happened sentiment highlight the sector, put so much just specify the openness, or what if not the key to Nevada party to push was part of, highlight the key console emotion represent your task, which you Marisota mirror area but it could not be trial assistance.",
+            timeStamp: 3
+        },
+        {
+            name: "Aditya Nair",
+            shitHeSaid: "Yeah yeah tried it. Yeah, you bet mitofit Whoa, yeah Becky Jackie up natural recording and post or during or after the kangaroo playable Ragamala basically recordings or play chiringuito mapic is becoming a key player to EBA and then luggage will be born.",
+            timestamp: 4
+        }
+    ]
+
+    function addItemsToList(name, shitHeSaid, timestamp){
+        var
+    }
+    return ( 
+        null
+    );
+}
+ 
+export default Transcript;
